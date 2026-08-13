19cf620ad4fb072122d50622e96c8a8842ae55a0
Daniel Wu <danielwu288@gmail.com>
2026-08-12 21:41:59 -0300

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.cpp
@@ -0,0 +1,35 @@
+class MinStack {
+private:
+    vector<int> stack;
+    vector<int> minimum;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if (minimum.empty() || val <= minimum.back()) {
+            minimum.push_back(val);
+        }
+        else {
+            minimum.push_back(min(minimum.back(), val));
+        }
+
+        stack.push_back(val);
+    }
+    
+    void pop() {
+        minimum.pop_back();
+        stack.pop_back();
+    }
+    
+    int top() {
+        return stack.back();
+    }
+    
+    int getMin() {
+        int value = minimum.back();
+        
+        return value;
+    }
+};
